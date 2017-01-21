cd858a0e7bcf84d8c1898f4e4c6f461bd6cf3b9f
Wolly <wolly.m.b@gmail.com>
2017-01-21 01:52:12 +0100

    no message

--- sourcefiles/GXPEngine/MyGame.cs
+++ sourcefiles/GXPEngine/MyGame.cs
@@ -4,193 +4,185 @@ using System.Drawing;
 using GXPEngine;
 
 public class MyGame : Game //MyGame is a Game
-{	private List<Sprite> _colidables;
-	private Canvas _canvas1;
-	private Canvas _canvas2;
-	private Player _player1;
-	private Player _player2;
-	private int _size1;
-	private int _size2;
-	private bool _active1;
-	private bool _active2;
-	private int _wait1;
-	private int _wait2;
-	private float _x1;
-	private float _y1;
-	private float _x2;
-	private float _y2;
-	private Circle _circle1;
-	private Circle _circle2;
+{
+    private List<Sprite> _collidables;
+
+    private Player _player1;
+    private Player _player2;
 
 
-	public MyGame () : base(800, 600, false, false)
-	{
-		_colidables = new List<Sprite>();
-		targetFps = 60;
-		_player1 = new Player("test1.png");
-		_player2 = new Player("test2.png");
-		AddChild(_player1);
-		AddChild(_player2);
-		_circle1 = new Circle(0,0,0);
-		_circle2 = new Circle(0, 0, 0);
-		_size1 = 10;
-		_size2 = 10;
-		_canvas1 = new Canvas(800, 600);
-		AddChild(_canvas1);
-		_canvas2 = new Canvas(800, 600);
-		AddChild(_canvas2);
-	}
+    public MyGame () : base(800, 600, false, false)
+    {
+        _collidables = new List<Sprite>();
+        targetFps = 60;
+        _player1 = new Player("test1.png");
+        _player2 = new Player("test2.png");
+        AddChild(_player1);
+        AddChild(_player2);
+    }
 
-	void Update ()
-	{
-		PlayerMovement();
-		if (Input.GetMouseButtonDown(1))
-		{
-			_player1.Position.y = Input.mouseY;
-			_player1.Position.x = Input.mouseX;
-			_player1.alpha = 100;
-		}
-		if (Input.GetMouseButtonDown(2))
-		{
-			_player2.Position.y = Input.mouseY;
-			_player2.Position.x = Input.mouseX;
-			_player2.alpha = 100;
-		}
-		if (Input.GetKeyDown(Key.R))
-		{
-			Bullet bullet = new Bullet();
-			bullet.x = _player1.x;
-			bullet.y = _player1.y;
-			AddChild(bullet);
-			_colidables.Add(bullet);
-		}
+    void Update ()
+    {
+        PlayerMovement();
 
-		if (Input.GetKeyDown(Key.NUMPAD_7))
-		{ 
-			Bullet bullet = new Bullet();
-			bullet.x = _player2.x;
-			bullet.y = _player2.y;
-			AddChild(bullet);
-			_colidables.Add(bullet);
-		}
-		if (Input.GetKeyDown(Key.V))
-		{
-			_active1 = true;
-			_x1 = _player1.x;
-			_y1 = _player1.y;
-			CreateThemWaves(_x1, _y1, ref _size1, new Pen(Color.Red),_circle1,_canvas1);
-		}
-		if (Input.GetKeyDown(Key.NUMPAD_1))
-		{
-			_active2= true;
-			_x2 = _player2.x;
-			_y2 = _player2.y;
-			CreateThemWaves(_x2, _y2, ref _size2, new Pen(Color.Green),_circle2,_canvas2);
-		}
-		if (Input.GetKeyDown(Key.SEMICOLON))
-		{ 
-		}
+        if (Input.GetMouseButtonDown(1))
+        {
+            _player1.Position.y = Input.mouseY;
+            _player1.Position.x = Input.mouseX;
+            _player1.alpha = 100;
+        }
 
-		if (_active1)
-		{
+        if (Input.GetMouseButtonDown(2))
+        {
+            _player2.Position.y = Input.mouseY;
+            _player2.Position.x = Input.mouseX;
+            _player2.alpha = 100;
+        }
 
-			CheckIfInCircle(_circle1);
+        if (Input.GetKeyDown(Key.R))
+        {
+            Bullet bullet = new Bullet();
+            bullet.x = _player1.x;
+            bullet.y = _player1.y;
+            AddChild(bullet);
+            _collidables.Add(bullet);
+        }
 
-			if(_wait1==10)
-			{
-				CreateThemWaves(_x1, _y1, ref _size1,new Pen(Color.Red),_circle1,_canvas1);
-				if (_size1 >= 510)
-				{
-					_size1 = 10;
-					_active1 = false;
-					_canvas1.graphics.Clear(Color.Transparent);
-					//move the circle reset to a function, posibly that also does the clears
-					_circle1.Position=Vec2.zero;
-					_circle1.Size = 0;
-					_player1.alpha = 100;
-					_player2.alpha = 100;
-				}
-				_wait1 = 0;
-			}
-			_wait1++;
-		}
+        if (Input.GetKeyDown(Key.NUMPAD_7))
+        { 
+            Bullet bullet = new Bullet();
+            bullet.x = _player2.x;
+            bullet.y = _player2.y;
+            AddChild(bullet);
+            _collidables.Add(bullet);
+        }
 
-		if (_active2)
-		{
+        if (Input.GetKeyDown(Key.V))
+        {
+            _player1.Active = true;
+            _player1.X = _player1.x;
+            _player1.Y = _player1.y;
+            CreateWaves(_player1, new Pen(Color.Red));
+        }
 
-			CheckIfInCircle(_circle2);
+        if (Input.GetKeyDown(Key.NUMPAD_1))
+        {
+            _player2.Active = true;
+            _player2.X = _player2.x;
+            _player2.Y = _player2.y;
+            CreateWaves(_player2, new Pen(Color.Green));
+        }
 
-			if (_wait2 == 10)
-			{
-				CreateThemWaves(_x2, _y2, ref _size2, new Pen(Color.Green),_circle2,_canvas2);
-				if (_size2 >= 510)
-				{
-					_size2 = 10;
-					_active2 = false;
-					_canvas2.graphics.Clear(Color.Transparent);
-					//move the circle reset to a function, posibly that also does the clears
-					_circle2.Position = Vec2.zero;
-					_circle2.Size = 0;
-					_player1.alpha = 100;
-					_player2.alpha = 100;
-				}
-				_wait2 = 0;
-			}
-			_wait2++;
-		}
+        if (Input.GetKeyDown(Key.SEMICOLON))
+        { 
 
-	}
+        }
 
-	private void CreateThemWaves(float pX, float pY,ref int pSize,Pen pen,Circle circle,Canvas canvas)
-	{
-		canvas.graphics.Clear(Color.Transparent);
-		circle.Size = pSize;
-		circle.Position.x = pX;
-		circle.Position.y = pY;
-		canvas.graphics.DrawEllipse(pen,pX-pSize/2, pY- pSize / 2, pSize, pSize);
-		pSize += 100;
-	}
+        if (_player1.Active)
+        {
+            CheckIfInCircle();
 
-	private void CheckIfInCircle(Circle pCircle)
-	{
-		if (pCircle.Position.DistanceTo(_player1.Position) <= pCircle.Size/2)
-			_player1.alpha=10;
-		if (pCircle.Position.DistanceTo(_player2.Position) <= pCircle.Size / 2)
-			_player2.alpha = 10;
-	}
+            if(_player1.Wait == 10)
+            {
+                CreateWaves(_player1, new Pen(Color.Red));
+                _player1.Size += 100;
+                if (_player1.Size >= 510)
+                {
+                    _player1.Size = 10;
+                    _player1.Active = false;
+                    _player1.Canvas.graphics.Clear(Color.Transparent);
+                    //move the circle reset to a function, posibly that also does the clears
+                    _player1.Wave.Position = Vec2.zero;
+                    _player1.Wave.Size = 0;
+                    _player1.alpha = 100;
+                    _player2.alpha = 100;
+                }
+                _player1.Wait = 0;
+            }
+            _player1.Wait++;
+        }
 
-	private void PlayerMovement()
-	{
-		if (Input.GetKey(Key.W))
-			_player1.Position.y -= 10;
-		if (Input.GetKey(Key.S))
-			_player1.Position.y += 10;
-		if (Input.GetKey(Key.A))
-			_player1.Position.x -= 10;
-		if (Input.GetKey(Key.D))
-			_player1.Position.x += 10;
-		if (Input.GetKey(Key.UP))
-			_player2.Position.y -= 10;
-		if (Input.GetKey(Key.DOWN))
-			_player2.Position.y += 10;
-		if (Input.GetKey(Key.LEFT))
-			_player2.Position.x -= 10;
-		if (Input.GetKey(Key.RIGHT))
-			_player2.Position.x += 10;
-	}
+        Console.WriteLine("Player1 X,Y = " + _player1.X + ", " + _player1.Y + " ~ " + "Player1 x,y = " + _player1.x + ", " +  _player1.y);
+        Console.WriteLine(_player1.Wave.Position.x + ", " + _player1.Wave.Position.y);
 
-	//system starts here
+        if (_player2.Active)
+        {
+            CheckIfInCircle();
 
-	public int CheckCollision(Sprite other)
-	{
-		foreach (Sprite sprite in _colidables)
-		{
+            if (_player2.Wait == 10)
+            {
+                CreateWaves(_player2, new Pen(Color.Green));
+                _player2.Size += 100;
+                if (_player2.Size >= 510)
+                {
+                    _player2.Size = 10;
+                    _player2.Active = false;
+                    _player2.Canvas.graphics.Clear(Color.Transparent);
+                    //move the circle reset to a function, posibly that also does the clears
+                    _player2.Wave.Position = Vec2.zero;
+                    _player2.Wave.Size = 0;
+                    _player1.alpha = 100;
+                    _player2.alpha = 100;
+                }
+                _player2.Wait = 0;
+            }
+            _player2.Wait++;
+        }
 
-		}
-		return 0;
-	}
-	static void Main() 
-	{
-		new MyGame().Start();
-	}
+    }
+
+    private void CreateWaves(Player pPlayer, Pen pPen)
+    {
+        //float pX, float pY, Pen pen, Circle circle, Canvas canvas
+        pPlayer.Canvas.graphics.Clear(Color.Transparent);
+        pPlayer.Wave.Position.x = pPlayer.X;
+        pPlayer.Wave.Position.y = pPlayer.Y;
+        pPlayer.Canvas.graphics.DrawEllipse(pPen, pPlayer.X - pPlayer.Size / 2, pPlayer.Y - pPlayer.Size / 2, pPlayer.Size, pPlayer.Size);
+    }
+
+    private void CheckIfInCircle()
+    {
+        if (_player1.Wave.Position.DistanceTo(_player1.Position) <= _player1.Size / 2)
+        {
+            _player2.alpha = 1;
+        }
+        if (_player2.Wave.Position.DistanceTo(_player2.Position) <= _player2.Size / 2)
+        {
+            _player1.alpha = 1;
+        }
+    }
+
+    private void PlayerMovement()
+    {
+        if (Input.GetKey(Key.W))
+            _player1.Position.y -= 10;
+        if (Input.GetKey(Key.S))
+            _player1.Position.y += 10;
+        if (Input.GetKey(Key.A))
+            _player1.Position.x -= 10;
+        if (Input.GetKey(Key.D))
+            _player1.Position.x += 10;
+        if (Input.GetKey(Key.UP))
+            _player2.Position.y -= 10;
+        if (Input.GetKey(Key.DOWN))
+            _player2.Position.y += 10;
+        if (Input.GetKey(Key.LEFT))
+            _player2.Position.x -= 10;
+        if (Input.GetKey(Key.RIGHT))
+            _player2.Position.x += 10;
+    }
+
+    //system starts here
+    public int CheckCollision(Sprite other)
+    {
+        foreach (Sprite sprite in _collidables)
+        {
+
+        }
+        return 0;
+    }
+    static void Main() 
+    {
+        new MyGame().Start();
+    }
 }
--- sourcefiles/GXPEngine/Player.cs
+++ sourcefiles/GXPEngine/Player.cs
@@ -8,11 +8,56 @@ namespace GXPEngine
 		public Vec2 Reticle;
 		public Reticle1 _reticle1;
 		public Reticle2 _reticle2;
+
+		private Canvas _canvas;
+		private Circle _wave;
+		private int _size;
+		private bool _active;
+		private int _wait;
+		private float _x;
+		private float _y;
+
+		public Canvas Canvas {
+			get { return _canvas; }
+			set { _canvas = value; }
+		}
+		public Circle Wave {
+			get { return _wave; }
+			set { _wave = value; }
+		}
+		public int Size {
+			get { return _size; }
+			set { _size = value; }
+		}
+		public bool Active {
+			get { return _active; }
+			set { _active = value; }
+		}
+		public int Wait {
+			get { return _wait; }
+			set { _wait = value; }
+		}
+		public float X {
+			get { return _x; }
+			set { _x = value; }
+		}
+		public float Y {
+			get { return _y; }
+			set { _y = value; }
+		}
+
+
 		public Player(string filename):base(filename)
 		{
 			Position = new Vec2();
 			Reticle = new Vec2(game.width / 2, game.height / 2);
 			SetOrigin(width / 2, height / 2);
+
+			_wave = new Circle(0, 0, 0);
+			_size = 10;
+			_canvas = new Canvas(800, 600);
+			game.AddChild(_canvas);
+
 			if (filename == "test1.png")
 			{
 				_reticle1 = new Reticle1();
@@ -29,6 +74,11 @@ namespace GXPEngine
 		{
 			x = Position.x;
 			y = Position.y;
+		}
+
+		public int GetSize()
+		{
+			return _size;
 		}
 	}
 }
